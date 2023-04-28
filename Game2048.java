aab899dc3963fcc2b5f650e1e9ac10105694056f
Mateusz Kulec <101333426+MateuszRL96@users.noreply.github.com>
2023-04-28 10:11:54 +0200

    Update Game2048.java

--- Game2048.java
+++ Game2048.java
@@ -4,9 +4,27 @@ import com.codegym.engine.cell.*;
 
 public class Game2048 extends Game{
     private static final int SIDE = 4;
+    private int[][] gameField = new int[SIDE][SIDE];
     
     @Override
     public void initialize(){
         setScreenSize(SIDE, SIDE);
+        createGame();
+        
+        //->last
+        drawScene();
+    }
+    
+    private void createGame(){
+        
+    }
+    
+    private void drawScene(){
+        for(int x = 0; x < SIDE; x++){
+            for(int y = 0; y < SIDE; y++)
+            {
+                setCellColor(x, y, Color.GREEN);
+            }
+        }
     }
 }
