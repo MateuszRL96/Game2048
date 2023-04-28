ffc61f61a44a7ba4b2fed7b449154422f833ac55
Mateusz Kulec <101333426+MateuszRL96@users.noreply.github.com>
2023-04-28 09:43:05 +0200

    Update Game2048.java

--- Game2048.java
+++ Game2048.java
@@ -3,4 +3,10 @@ import com.codegym.engine.cell.*;
 
 
 public class Game2048 extends Game{
+    private static final int SIDE = 4;
+    
+    @Override
+    public void initialize(){
+        setScreenSize(SIDE, SIDE);
+    }
 }
